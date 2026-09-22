ba07cc4b8aa15bc9f51632c4ebf59138a29358cf
Artemov Ivan (ZOXEXIVO) <zoxexivo@gmail.com>
2026-09-22 22:06:51 +0300

    Fix attrs (FM-Inside)

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -47,7 +47,7 @@ use serde_json::{Map, Value};
 
 const OUTPUT_VERSION: &str = "1.0";
 const DEFAULT_DATA_DIR: &str = "../data";
-const DEFAULT_OUT_FILE: &str = r"D:\Projects\open-football\src\database\src\data\database.db";
+const DEFAULT_OUT_FILE: &str = r"F:\Rust\open-football\src\database\src\data\database.db";
 
 struct Args {
     data_dir: PathBuf,
